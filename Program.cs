3c6ad2f2c7a60acea054b54b4c20cfeab021a8e7
Wendy Lemus <nicolas.bohorquez@anm.gov.co>
2025-06-19 16:29:03 -0500

    CI version GHRCR 4

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezin", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
 app.MapGet("/weatherforecast", () =>
